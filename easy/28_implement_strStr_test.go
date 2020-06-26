6031ef0af3adb749ef7d8aa32346eb4bfdf614dc
fengyuwusong <602766695@qq.com>
2020-06-26 23:06:21 +0800

    feat: leetcode 28/167/344/561/151

--- easy/28_implement_strStr_test.go
+++ easy/28_implement_strStr_test.go
@@ -51,9 +51,63 @@ func strStr(haystack string, needle string) int {
 	return -1
 }
 
+// kmp
+func strStr2(haystack, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	if len(needle) > len(haystack) {
+		return -1
+	}
+	next := buildNext(needle)
+	i, j := 0, 0
+
+	for i < len(haystack) && j < len(needle) {
+		if j == -1 || haystack[i] == needle[j] {
+			i++
+			j++
+		} else {
+			j = next[j]
+		}
+	}
+	if j == len(needle) {
+		return i - j
+	}
+	return -1
+}
+
+func buildNext(s string) []int {
+	// 初始化next数组
+	next := make([]int, len(s))
+	// 第一位赋值为-1 由于存在 j= next[j] 如第一位赋值0则会产生死循环
+	next[0] = -1
+	// 主串指针
+	i := 0
+	j := -1 // 模式串指针 赋值-1 与上面同理
+	for i < len(s)-1 {
+		if j == -1 || s[i] == s[j] {
+			// 模式串处于重开状态 或 当前字符相等 当前字符前进 赋值next表
+			i++
+			j++
+			// 重开状态 next[i]=0 即主串当前字符并不是子串开头
+			next[i] = j
+		} else {
+			// 否则模式串起始位置回归上一个最长前缀后缀相同位置
+			j = next[j]
+		}
+	}
+	return next
+}
+
 func TestStrStr(t *testing.T) {
 	println(strStr("hello", "ll"))
 	println(strStr("aaaaa", "bba"))
 	println(strStr("aaaaa", "aa"))
 	println(strStr("mississippi", "issipi"))
+
+	println(strStr2("hello", "ll"))
+	println(strStr2("aaaaa", "bba"))
+	println(strStr2("aaaaa", "aa"))
+	println(strStr2("mississippi", "issipi"))
+	println(strStr2("mississippi", ""))
 }
