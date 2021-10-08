4d077156e6a2eaad65d5c98ec16292992f6a8ba2
fengyuwusong <602766695@qq.com>
2021-10-09 00:27:33 +0800

    feat: finish easy 19、876

---
+++ easy/876. 链表的中间结点/main.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+/**
+给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+
+如果有两个中间结点，则返回第二个中间结点。
+
+
+
+示例 1：
+
+输入：[1,2,3,4,5]
+输出：此列表中的结点 3 (序列化形式：[3,4,5])
+返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+示例 2：
+
+输入：[1,2,3,4,5,6]
+输出：此列表中的结点 4 (序列化形式：[4,5,6])
+由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+
+
+提示：
+
+给定链表的结点数介于 1 和 100 之间。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/middle-of-the-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 1. 存储数组 取中间节点
+// 2. 单节点2次遍历
+// 3. 双节点快慢遍历 快的速度是慢的两倍则慢的则为中点
+func middleNode(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil {
+		if fast.Next == nil {
+			break
+		}
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+	return slow
+}
+
+func main() {
+	node6 := ListNode{Val: 6, Next: nil}
+	node5 := ListNode{Val: 5, Next: &node6}
+	node4 := ListNode{Val: 4, Next: &node5}
+	node3 := ListNode{Val: 3, Next: &node4}
+	node2 := ListNode{Val: 2, Next: &node3}
+	node1 := ListNode{Val: 1, Next: &node2}
+	fmt.Println(middleNode(&node1).Val)
+}
